85950d83bf62c6aeccd3501074fefd56aa596578
Cooper <leon.cooper@gceducationandskills.ac.uk>
2023-01-25 16:00:23 +0000

    added activities

--- day3.js
+++ day3.js
@@ -1,8 +1,8 @@
-let movies = [
-    "lord of the rings - the fellowship of the ring",
-    "lord of the rings - the two towers",
-    "blade runner"
-]
+// let movies = [
+//     "lord of the rings - the fellowship of the ring",
+//     "lord of the rings - the two towers",
+//     "blade runner"
+// ]
 // console.log("before", movies)
 // movies[2] = "star wars - new hope"
 // movies.push('star wars - a new hope')
@@ -36,3 +36,99 @@ let movies = [
 // let animals = ['cow', 'lion', 'cat']
 // animals.unshift('panda')
 // console.log(animal)
+
+
+// let favSongs = [
+//     "spice girls - wannabe",
+//     "oasis - wonderwall",
+//     "eiffel 65 - blue (da ba dee da ba dai)",
+// ]
+// for(let i = 0; i < favSongs.length; i++){
+//     console.log(favSongs[i], 'value of i is', i)
+// }
+
+
+// let multiplesTwo = []
+
+// for(let i = 1; i <= 20; i++){
+//     if(i % 2 === 0){
+//         multiplesTwo.push(i)
+//     }
+// }
+
+// console.log(`these are the multiples of 2 up to 20: ${multiplesTwo}`)
+
+// let num = 0
+// let multiplesTwo = []
+
+// while(num < 20){
+//     if(num % 2 === 0){
+//         multiplesTwo.push(num)
+//     }
+//     num++
+// }
+
+// console.log(`x2 up to 20: ${multiplesTwo}`)
+
+// let age = 14
+
+// while(age < 18){
+//     console.log(`you are ${age}, you are a child`)
+//     age++
+// }
+
+// console.log(`you are now ${age}, you are an adult`)
+
+// ACTIVITIES
+
+// Activity 1
+// let movies = [
+//     "lord of the rings - the fellowship of the ring",
+//     "lord of the rings - the two towers",
+//     "blade runner",
+//     "jurassic park",
+//     "lost world"
+// ]
+
+// movies.push("avatar")
+// movies.push("avatar 2")
+
+// for(let i = 0; i < movies.length; i++){
+//     console.log(i,':',movies[i])
+// }
+
+// Activity 2
+// for(let i = 1; i <= 6; i++){
+//     console.log(i, ':', Math.ceil(Math.random() * 49))
+// }
+
+// Activity 3
+// for(let i = 9; i >= 0; i--){
+//     console.log(i)
+// }
+
+// let num = 9
+// while(num >= 0){
+//     console.log(num)
+//     num--
+// }
+
+// Activity 4
+// let movies = [
+//     "lord of the rings - the fellowship of the ring",
+//     "lord of the rings - the two towers",
+//     "blade runner",
+//     "jurassic park",
+// ]
+
+// for(let i = 0; i < movies.length; i++){
+//     if(i === 2){
+//         if(movies[2] === 'ghostbusters'){
+//             console.log('yay its ghostbusters')
+//         }else{
+//             console.log('booo not ghostbusters')
+//         }
+//     }else{
+//         console.log(movies[i])
+//     }
+// }
