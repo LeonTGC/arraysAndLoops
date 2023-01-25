527fe1f09c953317be016b1c909f49d432486a8e
Cooper <leon.cooper@gceducationandskills.ac.uk>
2023-01-25 12:05:30 +0000

    added array methods

---
+++ day3.js
@@ -0,0 +1,28 @@
+let movies = [
+    "lord of the rings - the fellowship of the ring",
+    "lord of the rings - the two towers",
+    "blade runner"
+]
+// console.log("before", movies)
+// movies[2] = "star wars - new hope"
+// movies.push('star wars - a new hope')
+// movies.pop()
+// console.log(movies)
+
+
+// map
+// let names = ['leon', 'chris', 'ben', 'dave']
+// names.map((name) => {
+//     console.log('hello', name)
+// })
+
+// // splice
+// let nums = ['one', 'two', 'five', 'six', 'seven']
+// nums.splice(2, 2, 'three', 'four')
+// console.log(nums)
+
+
+// // slice
+// let food = ['eggs', 'oat milk', 'bananas', 'chocolate', 'bread']
+// let items = food.slice(2, 4)
+// console.log(items)
